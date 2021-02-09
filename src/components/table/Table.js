c4a2d6b3527496f9b7f4c491f05038d238f92d39
marselabdullin <abdmars@mail.ru>
2021-02-09 22:14:22 +0300

    finish table resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,8 +1,22 @@
 import {ExcelComponents} from '@core/ExcelComponents'
 import {createTable} from '@core/table.template'
+import {resizeHandler} from '@/components/table/table.resize'
+import {shouldResize} from '@/components/table/table.functions'
 
 export class Table extends ExcelComponents {
     static className = 'excel__table'
+
+    constructor($root) {
+        super($root, {
+            listeners: ['mousedown'],
+        })
+    }
+
+    onMousedown(event) {
+        if (shouldResize(event)) {
+            resizeHandler(event, this.$root)
+        }
+    }
 
     toHTML() {
         return createTable(30)
